572a9da4a43d0fec83f43dd43ce3127839eca500
yuramishin <mishin.inbox@gmail.com>
2018-08-27 20:34:07 +0300

    Add MainTest.

---
+++ core/src/test/java/start/MainTest.java
@@ -0,0 +1,54 @@
+package start;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Class MainTest.
+ * Tests Main class.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 27.08.2018
+ */
+public class MainTest {
+    /**
+     * Method tests new object creation.
+     */
+    @Test
+    public void whenNewObjThenReturnName() throws Exception {
+        String expected = "Main";
+        String result = new Main().getClass().getSimpleName();
+
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Method tests app launch.
+     */
+    @Test
+    public void whenRunMain() throws Exception {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        String param = "Yura";
+        Main.main(param);
+
+        System.setOut(stdout);
+
+        String result = new String(out.toByteArray());
+        String expected = new StringBuilder()
+                .append("Hello, ")
+                .append(param)
+                .append("!")
+                .append(System.lineSeparator())
+                .toString();
+
+        assertThat(result, is(expected));
+    }
+}
