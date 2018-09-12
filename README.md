9c05349d33b160087554fd549ad94721600fdac3
Yura Mishin <mishin.inbox@gmail.com>
2018-09-12 19:24:34 +0400

    Added simple servlet

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Welcome to Yura Mishin's Java repository.
 
 ![Photo](https://github.com/MishinCorp/java/blob/master/img/avatar.jpg)![Photo](https://github.com/MishinCorp/java/blob/master/img/java_logo.jpg)
 
-[Chapter 1. Java SE](https://github.com/MishinCorp/java/tree/master/core)
-
 ##### Useful resources:
 + [Oracle Help Center](https://docs.oracle.com/)
 + [Java-online.ru](http://java-online.ru/)
 + [Head First Java](https://www.oreilly.com/library/view/head-first-java/0596009208/)
 + [Thinking in Java (En)](https://en.wikipedia.org/wiki/Thinking_in_Java/) // [(Ru)](http://wikijava.it-cache.net/index.php)
 + [Core Java. Volume I, II](https://www.horstmann.com/corejava.html)
++ [Apache Maven](https://maven.apache.org/)
++ [Maven Repository: Top Projects at Maven Repository](https://mvnrepository.com/popular)
 + [PostgreSQL documentation](https://www.postgresql.org/docs/)
 + [Head First SQL](http://shop.oreilly.com/product/9780596526849.do)
 + [SQL in a Nutshell](http://shop.oreilly.com/product/9780596518851.do)
---
+++ core/src/main/java/helloworld/Servlet.java
@@ -0,0 +1,46 @@
+package helloworld;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Class Servlet.
+ * Implements simple html-page.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 12.09.2018
+ */
+public class Servlet extends HttpServlet {
+    @Override
+    protected final void doGet(final HttpServletRequest req,
+                               final HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        HelloUser helloUser = new HelloUser();
+        String result = helloUser.getGreeting("Yura");
+
+        StringBuilder html = new StringBuilder();
+        html.append("<!doctype html>")
+                .append("<html lang=\"en\">")
+                .append("<head>")
+                .append("<meta charset=\"UTF-8\">")
+                .append("<title>Document</title>")
+                .append("</head>")
+                .append("<body>")
+                .append("<p><a href=\"..\\index.jsp\">Главная</a></p>")
+                .append(result)
+                .append("</body>")
+                .append("</html>");
+
+        out.println(html.toString());
+
+        out.close();
+    }
+}
