f3f1f428edb6beba0ab15241fc0c51b71dcaac75
Yura Mishin <mishin.inbox@gmail.com>
2018-10-16 10:26:20 +0400

    Added an iterator example.

---
+++ collections/src/main/java/iterator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for iterator.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 16.10.2018
+ */
+package iterator;
---
+++ collections/src/test/java/iterator/JaggedArrayIteratorTest.java
@@ -0,0 +1,65 @@
+package iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Class JaggedArrayIteratorTest.
+ * Tests MatrixIterator class.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 16.10.2018
+ */
+public class JaggedArrayIteratorTest {
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new MatrixIterator(new int[][]{{1}, {3, 4}, {7}});
+    }
+
+    /**
+     * Tests next() method.
+     */
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    /**
+     * Tests hasNext() and next() methods.
+     */
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    /**
+     * Tests hasNext() and next() methods.
+     */
+    @Test
+    public void hasNextNextSequentialInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(false));
+    }
+}
