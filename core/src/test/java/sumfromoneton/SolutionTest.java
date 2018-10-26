b0ff4d297502ced9ea6849afd8d65b1d4692df26
Yura Mishin <mishin.inbox@gmail.com>
2018-10-26 07:52:17 +0400

    Added computing sum from 1 to N.

---
+++ core/src/test/java/sumfromoneton/SolutionTest.java
@@ -0,0 +1,27 @@
+package sumfromoneton;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Class SolutionTest.
+ * Tests Solution class.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 18.09.2018
+ */
+public class SolutionTest {
+
+    /**
+     * Tests sum() method.
+     */
+    @Test
+    public void whenInvokeThenSum() {
+        Solution solution = new Solution();
+
+        assertThat(solution.sum(3), is(6));
+        assertThat(solution.sum(10), is(55));
+    }
+}
